ac9c7d2a9ee4f7710ef37aeb41b3b94c6760ebdb
luolong <luolong@beyondbit.com>
2020-11-18 17:00:07 +0800

    imageView服务

---
+++ src/appointments/image-view.ts
@@ -0,0 +1,21 @@
+import { execute } from "../web-bridge/native";
+
+const serviceName = "imageView";
+
+/**
+ * 显示图像查看器
+ * @param imageList 要显示的图片列表
+ * @param currentIndex 当前的图片索引
+ */
+export async function show(imageList: { 
+    /**
+     * 图片的下载URL
+     */
+    url: string
+    /**
+     * 图片的标题
+     */
+    title: string
+}[], currentIndex: null) {
+    return await execute<any>(serviceName, "show", { imageList: imageList, currentIndex: currentIndex });
+}
