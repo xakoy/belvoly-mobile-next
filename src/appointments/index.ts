5613d17e2b87e688a11446b546fb8c50764ed903
luolong <luolong@beyondbit.com>
2020-11-19 19:22:42 +0800

    修改system导出错误，导出了plugin

--- src/appointments/index.ts
+++ src/appointments/index.ts
@@ -3,7 +3,7 @@ import * as driver from './driver'
 import * as imageView from './image-view'
 import * as location from './location'
 import * as plugin from './plugin'
-import * as system from './plugin'
+import * as system from './system'
 import * as webview from './webview'
 
 export { camera, driver, imageView, location, plugin, system, webview }
