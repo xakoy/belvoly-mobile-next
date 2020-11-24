b3545c616312b363da7725a32980800f00b054eb
luolong <luolong@beyondbit.com>
2020-11-24 19:08:09 +0800

    增加web-native.full.js编译导出

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,41 @@
 import commonjs from 'rollup-plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
 
-export default {
-    input: './index.ts',
-    output: {
-        file: 'dist/index.full.js',
-        format: 'umd',
-        name: 'umd'
-    },
-    plugins: [
-        typescript({
-            tsconfigOverride: {
-                compilerOptions: {
-                    declaration: false
+export default [
+    {
+        input: './index.ts',
+        output: {
+            file: 'dist/index.full.js',
+            format: 'umd',
+            name: 'umd'
+        },
+        plugins: [
+            typescript({
+                tsconfigOverride: {
+                    compilerOptions: {
+                        declaration: false
+                    }
                 }
-            }
-        }),
-        commonjs()
-    ]
-}
+            }),
+            commonjs()
+        ]
+    },
+    {
+        input: './web-native.ts',
+        output: {
+            file: 'dist/web-native.full.js',
+            format: 'umd',
+            name: 'umd'
+        },
+        plugins: [
+            typescript({
+                tsconfigOverride: {
+                    compilerOptions: {
+                        declaration: false
+                    }
+                }
+            }),
+            commonjs()
+        ]
+    }
+]
